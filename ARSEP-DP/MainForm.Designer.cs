3187aad6a99e644fff695b84d66f40c78cba3b11
Daniel Rodrigues <Daniel Rodrigues@DESKTOP-OT4P3MN>
2021-06-28 19:30:02 -0300

    Adicionar arquivos de projeto.

---
+++ ARSEP-DP/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ARSEP_DP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sle01 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sle01
+            // 
+            this.sle01.Location = new System.Drawing.Point(239, 156);
+            this.sle01.Name = "sle01";
+            this.sle01.Size = new System.Drawing.Size(75, 23);
+            this.sle01.TabIndex = 2;
+            this.sle01.Text = "Selecionar";
+            this.sle01.UseVisualStyleBackColor = true;
+            this.sle01.Click += new System.EventHandler(this.sle01_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sle01);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button sle01;
+    }
+}
+
---
+++ ARSEP-DP/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ARSEP_DP
+{
+
+
+    public partial class MainForm : Form
+    {
+        string fileContent = string.Empty;
+        string filePath = string.Empty;
+        public MainForm()
+        {
+            InitializeComponent();        }
+
+      
+
+        private void findPDF_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void sle01_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+
+        }
+    }
+}
